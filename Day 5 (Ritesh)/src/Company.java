39871b4c544615431c659de7a1ef06b960b49e1d
harshita95 <hmarwah60@gmail.com>
2017-07-25 10:04:18 +0530

    Add files via upload

---
+++ Day 5 (Ritesh)/src/Company.java
@@ -0,0 +1,15 @@
+/**
+ * Created by marwahh on 7/25/2017.
+ */
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.CLASS)
+@Target(ElementType.TYPE)
+@interface Company{
+    String name();
+    String location();
+}
